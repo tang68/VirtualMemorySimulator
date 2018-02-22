53a209a8e868fe58d1d55caba3c5e37677a49bd1
Ryan Phat Tang <ryanphattang@Ryans-Mac-mini.local>
2018-02-22 09:11:33 -0800

    testting CPU

--- src/cs431p2/CPU.java
+++ src/cs431p2/CPU.java
@@ -1,5 +1,35 @@
 package cs431p2;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class CPU {
+
+	public static void main(String[] args) {
+		
+		Scanner scanInput = new Scanner(System.in);
+		System.out.println("Enter test file name: ");
+//		String testFile = scanInput.next();
+//		String testFilePath = "Project2_test_and_page_files/test_files/" + testFile;
+		String testFilePath = "Project2_test_and_page_files/test_files/test_1.txt";
+		
+		Scanner scanFiles = null;
+		try {
+			scanFiles = new Scanner(new FileInputStream(testFilePath));
+
+			/** THE MEAT */
+			while (scanFiles.hasNextLine()) {
+				
+			}
+
+		} catch (FileNotFoundException ex) {
+			System.out.println("***** Unable to open file");
+		} finally {
+			scanFiles.close();
+			scanInput.close();
+		}
+		
+
+	}
 
 }
